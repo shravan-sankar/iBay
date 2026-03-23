bdd62ad242e2d82d0ce9e9637e70a0e5ecdb3549
Ashley Stokes <stokesash30@gmail.com>
2026-03-23 16:52:46 +0000

    changes

--- frontend/js/browse.js
+++ frontend/js/browse.js
@@ -4,7 +4,7 @@ const products = [
   { name: "Item 2", price: 20, seller: "Bob", category: "Clothes", image: "" }
 ];
 
-const container = document.getElementById("products-container");
+const Reccontainer = document.getElementById("products-container");
 
 
 products.forEach(product => {
@@ -19,5 +19,29 @@ products.forEach(product => {
     <p>${product.category}</p>
   `;
 
-  container.appendChild(card);
+  Reccontainer.appendChild(card);
 });
+
+const latestproducts = [
+  { name: "Item 1", price: 10, seller: "Alice", category: "Tech", image: "" },
+  { name: "Item 2", price: 20, seller: "Bob", category: "Clothes", image: "" }
+];
+
+const Latcontainer = document.getElementById("latest-products-container");
+
+
+products.forEach(product => {
+  const card = document.createElement("div");
+  card.className = "product-card";
+
+  card.innerHTML = `
+    <img src="${product.image}" class="product-image">
+    <h2>${product.name}</h2>
+    <p>£${product.price}</p>
+    <p>Seller: ${product.seller}</p>
+    <p>${product.category}</p>
+  `;
+
+  Latcontainer.appendChild(card);
+});
+
